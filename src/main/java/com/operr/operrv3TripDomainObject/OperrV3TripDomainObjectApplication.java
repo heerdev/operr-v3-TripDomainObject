663ab868b551d5a601a42b0fec10f02958d48b27
zubair <zubair@heersoftware.com>
2018-09-04 10:29:47 +0530

    release1

---
+++ src/main/java/com/operr/operrv3TripDomainObject/OperrV3TripDomainObjectApplication.java
@@ -0,0 +1,12 @@
+package com.operr.operrv3TripDomainObject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperrV3TripDomainObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperrV3TripDomainObjectApplication.class, args);
+	}
+}
